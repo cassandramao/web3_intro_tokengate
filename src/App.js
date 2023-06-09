b5ce07b71e07fc12a018e9d5fde95d63d917f3c3
cassandramao <136063718+cassandramao@users.noreply.github.com>
2023-06-09 01:56:36 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   state = { 
     web3: null,
     accounts: null,
-    contract: 'CONTRACT ADDRESS HERE',
+    contract: '0x4bbf090922279222f26a696634bb6b40f78d2107',
     hasNft: false
   };
 
